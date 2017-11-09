d3bd5b3bbd47ea2ce91e07eebe281e96199d5553
Kevin-Wan <xiankai.wan@gmail.com>
2017-11-09 16:32:57 +0800

    Fix: 测试轮播效果

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+    	//一瓶所能看到的图片的数量
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
+//https://github.com/OwlFonk/OwlCarousel
+//http://www.owlcarousel.owlgraphic.com/docs/api-options.html
